3b654edf29790b18acaef1f2dac9496b992214f4
Jacob Frericks <jacob.frericks@gmail.com>
2019-10-22 10:08:50 -0500

    Adding app toolbar

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Mid Iowa Vending',
+      home: MyHomePage(title: 'Mid Iowa Vending'),
     );
   }
 }
@@ -59,53 +47,66 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Stack(
+      children: <Widget>[
+        new Container(
+          height: MediaQuery.of(context).size.height / 2,
+          width: double.infinity,
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage("assets/banner_1.jpeg"),
+              fit: BoxFit.cover,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Container(
+          color: Color.fromRGBO(60, 60, 60, 0.19),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: new AppBar(
+            title: new Text(widget.title, style: TextStyle(fontSize: 25)),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            actions: <Widget>[
+              GestureDetector(
+                onTap: () { print("Home was tapped!"); },
+                child: Center(child: Text("Home", style: TextStyle(fontSize: 20))),
+              ),
+              GestureDetector(
+                onTap: () { print("Vending Products was tapped!"); },
+                child: Center(child:
+                  Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Text("Vending Products", style: TextStyle(fontSize: 20))
+                  )
+                ),
+              ),
+              GestureDetector(
+                onTap: () { print("Vending Services was tapped!"); },
+                child: Center(child:
+                  Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Text("Vending Services", style: TextStyle(fontSize: 20))
+                  )
+                ),
+              ),
+              GestureDetector(
+                onTap: () { print("Contact Us was tapped!"); },
+                child: Center(child:
+                  Padding(
+                      padding: EdgeInsets.only(left: 8, right: 8),
+                      child: Text("Contact Us", style: TextStyle(fontSize: 20))
+                  )
+                ),
+              ),
+            ],
+          ),
+          body: new Container(
+            color: Colors.transparent,
+          ),
+        ),
+      ],
     );
   }
 }

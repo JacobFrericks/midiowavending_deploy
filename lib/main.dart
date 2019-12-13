8dc9d403224b9563c40ece335244b3f4be677e29
Jacob Frericks <jacob.frericks@gmail.com>
2019-12-12 23:56:47 -0600

    debugging github pages

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mid Iowa Vending',
+      title: 'Mid Iowa Vennding',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Mid Iowa Vending'),
+      home: MyHomePage(title: 'Mid Iowa Vennding'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 

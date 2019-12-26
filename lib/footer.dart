a811243509f5798f3e014895b407b17ef4a2fa9e
Jacob Frericks <jacob.frericks@gmail.com>
2019-12-26 16:54:07 -0600

    completing footer

--- lib/footer.dart
+++ lib/footer.dart
@@ -10,18 +10,37 @@ class Footer extends StatefulWidget {
 class _ScreenState extends State<Footer> {
   @override
   Widget build(BuildContext context) {
-    return contactUsButton();
+    return Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+          ),
+          SelectableText(
+              "Contact us today to learn more about our reliable vending services, and 100% satisfaction guarantee",
+              style: TextStyle(fontSize: 15.0),
+            textAlign: TextAlign.center,
+          ),
+          contactUsButton(),
+        ]
+    );
   }
 
   Widget contactUsButton() {
-    return  OutlineButton(
-      onPressed: (){
-        print("YAY!");
+    return FlatButton(
+      color: Color.fromRGBO(0, 128, 128, 1),
+      textColor: Colors.white,
+      padding: EdgeInsets.all(8.0),
+      onPressed: () {
+        print("Contact Us Pressed!");
       },
-      child: SelectableText("CONTACT US", style: TextStyle(color: Colors.white, fontSize: 15)),
-      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(300)),
-      borderSide: BorderSide(color: Colors.white, width: 3),
-      highlightColor: Colors.red,
+      child: Text(
+        "CONTACT US",
+        style: TextStyle(fontSize: 15.0),
+      ),
+      shape: RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(18.0),
+      ),
+
     );
   }
 }
